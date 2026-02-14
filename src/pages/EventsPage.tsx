a0e6a3ee44b497578d0549025a00fdd7a401f870
hermes <a28704@svalero.com>
2026-02-14 10:50:29 +0100

    Enlazada página detalle al clickar en la tarjeta

--- src/pages/EventsPage.tsx
+++ src/pages/EventsPage.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import type { Event } from "../types/Event";
+import { NavLink } from "react-router-dom";
 
 export function EventsPage() {
   const [events, setEvents] = useState<Event[]>([]);
@@ -10,35 +11,32 @@ export function EventsPage() {
     /*Query params */
   }
   const [query, setQuery] = useState("");
-  const [categoryFilter,setCategoryFilter] = useState("all");
+  const [categoryFilter, setCategoryFilter] = useState("all");
   const [sortOrder, setSortOrder] = useState<"asc | desc">("asc");
 
-  const categoryOptions = Array.from(new Set(events.map((event) => event.category))).sort();
+  const categoryOptions = Array.from(
+    new Set(events.map((event) => event.category)),
+  ).sort();
 
   const normalizedQuery = query.trim().toLowerCase();
   const filteredEvents = events.filter((event) => {
-
-    if(categoryFilter !== "all" && event.category !== categoryFilter){
-        return false;
+    if (categoryFilter !== "all" && event.category !== categoryFilter) {
+      return false;
     }
 
-    if(normalizedQuery) return true;
+    if (normalizedQuery) return true;
 
-        const searchableText = [
-          event.name,
-          event.category,
-          event.price,
-        ]
-          .join(" ")
-          .toLowerCase();
+    const searchableText = [event.name, event.category, event.price]
+      .join(" ")
+      .toLowerCase();
 
-        return searchableText.includes(normalizedQuery);
-      });
+    return searchableText.includes(normalizedQuery);
+  });
 
-      const sortedEvents = [...filteredEvents].sort((a,b) => {
-        const comparison = a.name.localeCompare(b.name);
-        return sortOrder === "asc" ? comparison : -comparison;
-      });
+  const sortedEvents = [...filteredEvents].sort((a, b) => {
+    const comparison = a.name.localeCompare(b.name);
+    return sortOrder === "asc" ? comparison : -comparison;
+  });
 
   useEffect(() => {
     fetch("http://localhost:8080/events")
@@ -65,134 +63,137 @@ export function EventsPage() {
         minWidth: "1200px",
       }}
     >
-        <div
-    style={{
-      padding: "60px 80px",
-      maxWidth: "1400px",
-      margin: "0 auto",
-      boxSizing: "border-box",
-    }}
-  >
-      <h1
-        style={{
-          marginBottom: "32px",
-          color: "#562F00",
-          fontSize: "2.2rem",
-        }}
-      >
-        Eventos en Zaragoza
-      </h1>
-
       <div
         style={{
-          display: "flex",
-          alignItems: "center",
-          gap: "10px",
-          marginBottom: "20px",
-          flexWrap: "wrap",
+          padding: "60px 80px",
+          maxWidth: "1400px",
+          margin: "0 auto",
+          boxSizing: "border-box",
         }}
       >
-        <input
-          value={query}
-          onChange={(event) => setQuery(event.target.value)}
-          placeholder="Buscar por nombre, categoría, precio ..."
+        <h1
           style={{
-            flex: 1,
-            padding: "12px 14px",
-            borderRadius: "10px",
-            border: "1px solid #d0d7de",
-            background: "#f9fafb",
-            fontSize: "1rem",
-          }}
-        />
-        <select
-          value={categoryFilter}
-          onChange={(event) => setCategoryFilter(event.target.value)}
-          style={{
-            padding: "12px 14px",
-            borderRadius: "10px",
-            border: "1px solid #d0d7de",
-            backgroundColor: "#FFCE99",
-                color: "#562F00",
-            fontSize: "1rem",
-            minWidth: "170px",
-          }}
-        >
-          <option value="all">Todas las categorías</option>
-          {categoryOptions.map((category) => (
-            <option key={category} value={category}>
-              {category}
-            </option>
-          ))}
-        </select>
-        <select
-          value={sortOrder}
-          onChange={(event) => setSortOrder(event.target.value as "asc | desc")}
-          style={{
-            padding: "12px 14px",
-            borderRadius: "10px",
-            border: "1px solid #d0d7de",
-            backgroundColor: "#FFCE99",
+            marginBottom: "32px",
             color: "#562F00",
-            fontSize: "1rem",
-            minWidth: "170px",
+            fontSize: "2.2rem",
           }}
         >
-          <option value="asc">Ascendente (A-Z)</option>
-          <option value="desc">Descendente (Z-A)</option>
-        </select>
-        {query && (
-          <button
-            type="button"
-            onClick={() => setQuery("")}
+          Eventos en Zaragoza
+        </h1>
+
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "10px",
+            marginBottom: "20px",
+            flexWrap: "wrap",
+          }}
+        >
+          <input
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+            placeholder="Buscar por nombre, categoría, precio ..."
+            style={{
+              flex: 1,
+              padding: "12px 14px",
+              borderRadius: "10px",
+              border: "1px solid #d0d7de",
+              background: "#f9fafb",
+              fontSize: "1rem",
+            }}
+          />
+          <select
+            value={categoryFilter}
+            onChange={(event) => setCategoryFilter(event.target.value)}
             style={{
               padding: "12px 14px",
               borderRadius: "10px",
               border: "1px solid #d0d7de",
-              backgroundColor: "#562F00",
-                color: "#FFFDF1",
-              cursor: "pointer",
+              backgroundColor: "#FFCE99",
+              color: "#562F00",
+              fontSize: "1rem",
+              minWidth: "170px",
             }}
           >
-            Limpiar
-          </button>
-        )}
-      </div>
-
-      {filteredEvents.length === 0 ? (
-        <p style={{ color: "#666" }}>
-          Nose dispone de ningún evento con estas características
-        </p>
-      ) : (
-        <ul style={{ listStyle: "none", padding: 0 }}>
-          <div
+            <option value="all">Todas las categorías</option>
+            {categoryOptions.map((category) => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+          <select
+            value={sortOrder}
+            onChange={(event) =>
+              setSortOrder(event.target.value as "asc | desc")
+            }
             style={{
-              display: "grid",
-              gridTemplateColumns: "repeat(auto-fill, minmax(280px, 1fr))",
-              gap: "24px",
+              padding: "12px 14px",
+              borderRadius: "10px",
+              border: "1px solid #d0d7de",
+              backgroundColor: "#FFCE99",
+              color: "#562F00",
+              fontSize: "1rem",
+              minWidth: "170px",
             }}
           >
-            {sortedEvents.map((event) => (
-              <div
-                key={event.id}
-                style={{
-                  background: "#FFCE99",
-    borderRadius: "16px",
-    padding: "24px",
-    boxShadow: "0 8px 20px rgba(0,0,0,0.08)",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
-    minHeight: "170px",
-                }}
-              >
-                {/* Nombre */}
+            <option value="asc">Ascendente (A-Z)</option>
+            <option value="desc">Descendente (Z-A)</option>
+          </select>
+          {query && (
+            <button
+              type="button"
+              onClick={() => setQuery("")}
+              style={{
+                padding: "12px 14px",
+                borderRadius: "10px",
+                border: "1px solid #d0d7de",
+                backgroundColor: "#562F00",
+                color: "#FFFDF1",
+                cursor: "pointer",
+              }}
+            >
+              Borrar
+            </button>
+          )}
+        </div>
+
+        {filteredEvents.length === 0 ? (
+          <p style={{ color: "#666" }}>
+            Nose dispone de ningún evento con estas características
+          </p>
+        ) : (
+          <ul style={{ listStyle: "none", padding: 0 }}>
+            <div
+              style={{
+                display: "grid",
+                gridTemplateColumns: "repeat(auto-fill, minmax(280px, 1fr))",
+                gap: "24px",
+              }}
+            >
+              {sortedEvents.map((event) => (
+                <div
+                  key={event.id}
+                  style={{
+                    background: "#FFCE99",
+                    borderRadius: "16px",
+                    padding: "24px",
+                    boxShadow: "0 8px 20px rgba(0,0,0,0.08)",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-between",
+                    minHeight: "170px",
+                  }}
+                >
+                <NavLink to={`/events/${event.id}`}>    
+                  {/* Nombre */}
                   <h3
                     style={{
                       margin: 0,
-      color: "#562F00",
-      fontSize: "1.3rem",
-      fontWeight: 700,
+                      color: "#562F00",
+                      fontSize: "1.3rem",
+                      fontWeight: 700,
                     }}
                   >
                     {event.name}
@@ -207,46 +208,47 @@ export function EventsPage() {
                     Fecha: {event.eventDate}
                   </span>
 
-                {/* ETIQUETAS */}
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    alignItems: "center",
-                  }}
-                >
-                  <span
+                  {/* ETIQUETAS */}
+                  <div
                     style={{
-                      backgroundColor: "#562F00",
-                      color: "#FFFDF1",
-                      padding: "6px 14px",
-                      borderRadius: "20px",
-                      fontSize: "0.85rem",
-                      fontWeight: 600,
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
                     }}
                   >
-                    {event.category}
-                  </span>
+                    <span
+                      style={{
+                        backgroundColor: "#562F00",
+                        color: "#FFFDF1",
+                        padding: "6px 14px",
+                        borderRadius: "20px",
+                        fontSize: "0.85rem",
+                        fontWeight: 600,
+                      }}
+                    >
+                      {event.category}
+                    </span>
 
-                  <span
-                    style={{
-                      backgroundColor: "#562F00",
-                      color: "#FFFDF1",
-                      padding: "6px 14px",
-                      borderRadius: "20px",
-                      fontSize: "0.85rem",
-                      fontWeight: 600,
-                    }}
-                  >
-                    Precio: {event.price} €
-                  </span>
+                    <span
+                      style={{
+                        backgroundColor: "#562F00",
+                        color: "#FFFDF1",
+                        padding: "6px 14px",
+                        borderRadius: "20px",
+                        fontSize: "0.85rem",
+                        fontWeight: 600,
+                      }}
+                    >
+                      Precio: {event.price} €
+                    </span>
+                  </div>
+                </NavLink>
                 </div>
-              </div>
-            ))}
-          </div>
-        </ul>
-      )}
-    </div>
+              ))}
+            </div>
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
